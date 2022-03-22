993b793ceb5b2885c2ba5a530b9c4feb6b7b82ca
sasiman02 <gustndlfrl@gmail.com>
2022-03-22 11:54:51 +0900

    버튼을 만듬

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 function reddenPage() {
-
-    document.body;
+    let style = "position:fixed; width:60px; height:60px; bottom:40px; right:40px; background-color:#0C9; color:#FFF; border-radius:50px; text-align:center;	box-shadow: 2px 2px 3px #999;";
+    let toggleBtn = `<div id="speed-wagon" style="${style}">버튼</div>`;
+    document.body.innerHTML += toggleBtn;
 }
 
 chrome.action.onClicked.addListener((tab) => {
