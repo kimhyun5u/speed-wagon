19f482342041a3c50a3515b673ebe2bdeb25eae2
kimhyun5u <gustndlfrl@gmail.com>
2022-04-19 12:19:18 +0900

    add: youtube data api update

---
+++ popup.js
@@ -0,0 +1,15 @@
+let requestUrl = "https://www.googleapis.com/youtube/v3/search"
+let apiKey = "AIzaSyB54qFAt2iDi8peCN_oex6UZAtsJ1XumoI"
+
+function search() {
+    gapi.client.init({
+        'apiKey': apiKey,
+    }).then(function() {
+        return gapi.client.language.translations.list({
+            q: '잇섭',
+            // part: 'snippet'
+        })
+    }).then(function(response) {
+        console.log(response)
+    });
+}
