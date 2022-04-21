457f8780faad82c6d67a8de831b0551356865359
kimhyun5u <gustndlfrl@gmail.com>
2022-04-21 12:41:25 +0900

    update: list 업데이트

--- popup.js
+++ popup.js
@@ -9,6 +9,19 @@
         let xhreq = new XMLHttpRequest()
         xhreq.onreadystatechange = function() {
             console.log(xhreq.responseText)
+            let listEl = document.querySelector("#list")
+            listEl.innerHTML = ``
+            for(let item of xhreq.response.items) {
+                 let divEl = `<div class="d-flex item p-2">
+                 <div class="item-thumbsnail me-1" style="background-image: URL(${item.thumbnails.default.url});"></div>
+                 <div class="d-flex flex-column item-details">
+                     <div class="item-details-title">${item.snippet.title}</div>
+                     <div class="item-details-content">${item.snippet.descirption}</div>
+                 </div>
+             </div>`
+             listEl += innerHTML += divEl
+             
+            }
         }
         xhreq.open("GET", `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&key=AIzaSyB54qFAt2iDi8peCN_oex6UZAtsJ1XumoI`, true)
         xhreq.send()
