e2d0e78a19d80075cdf21cdf7643300ceb13a90d
kimhyun5u <gustndlfrl@gmail.com>
2022-04-21 11:46:26 +0900

    update: click listener

--- popup.js
+++ popup.js
@@ -1,20 +1,17 @@
 let requestUrl = "https://www.googleapis.com/youtube/v3/search"
 let apiKey = "AIzaSyB54qFAt2iDi8peCN_oex6UZAtsJ1XumoI"
 
-function search() {
-    
-    gapi.client.init({
-        'apiKey': apiKey,
-    }).then(function() {
-        return gapi.client.language.translations.list({
-            q: '잇섭',
-            // part: 'snippet'
-        })
-    }).then(function(response) {
-        console.log(response)
-    });
-}
-
 (function () {
-    document.querySelector("#")
+    document.querySelector("#search").addEventListener('click', function(target) {
+        gapi.client.init({
+            'apiKey': apiKey,
+        }).then(function() {
+            return gapi.client.language.translations.list({
+                q: '잇섭',
+                // part: 'snippet'
+            })
+        }).then(function(response) {
+            console.log(response)
+        });
+    });
 })()
