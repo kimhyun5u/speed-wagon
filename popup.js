a63eb1f2accb68f1c5b0a7a202211364ef1533c1
kimhyun5u <gustndlfrl@gmail.com>
2022-04-21 12:25:31 +0900

    update: typo 수정

--- popup.js
+++ popup.js
@@ -2,9 +2,9 @@
     document.querySelector("#search").addEventListener('click', function(target) {
         let q = "";
         
-        q.concat(document.querySelector("#productName"));
+        q = q.concat(document.querySelector("#productName"));
         
-        q.concat("리뷰");
+        q = q.concat("리뷰");
         
         let xhreq = new XMLHttpRequest()
         xhreq.onreadystatechange = function() {
