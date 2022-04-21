b552e0dfbfaaae3744cba50f7028af20b453b20f
kimhyun5u <gustndlfrl@gmail.com>
2022-04-21 12:19:58 +0900

    add: 제품 검색 기능 추가

--- popup.js
+++ popup.js
@@ -1,10 +1,16 @@
 (function () {
     document.querySelector("#search").addEventListener('click', function(target) {
+        let q = "";
+        
+        q.concat(document.querySElector("productName"));
+        
+        q.concat("리뷰");
+        
         let xhreq = new XMLHttpRequest()
         xhreq.onreadystatechange = function() {
             console.log(xhreq.responseText)
         }
-        xhreq.open("GET", "https://www.googleapis.com/youtube/v3/search?part=snippet&q=kpop+music&key=AIzaSyB54qFAt2iDi8peCN_oex6UZAtsJ1XumoI", true)
+        xhreq.open("GET", `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&key=AIzaSyB54qFAt2iDi8peCN_oex6UZAtsJ1XumoI`, true)
         xhreq.send()
     });
 })()
